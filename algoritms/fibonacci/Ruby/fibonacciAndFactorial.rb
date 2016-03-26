36344fbcf6fb923768ef810e5d8a0a146c4db028
galina <gbelonog@gmail.com>
2016-03-27 00:09:27 +0200

    added ruby

--- algoritms/fibonacci/Ruby/fibonacciAndFactorial.rb
+++ algoritms/fibonacci/Ruby/fibonacciAndFactorial.rb
@@ -6,7 +6,7 @@ def factorial n
     n > 1 ? n * factorial(n - 1) : 1
 end
 
-puts "Fibonacci numbers: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, … \n"
+puts "Fibonacci numbers: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, …  \n"
 puts "Enter an id of Fibonacci number: "
 
 number = gets.chomp.to_i

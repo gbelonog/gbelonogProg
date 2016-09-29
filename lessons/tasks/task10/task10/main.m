dbfbca8a2183199bfbe470c8f31f8fbfe1275dc9
galina <gbelonog@gmail.com>
2016-09-29 17:57:37 +0300

    tasks

--- lessons/tasks/task10/task10/main.m
+++ lessons/tasks/task10/task10/main.m
@@ -10,8 +10,18 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        char name[10], city[10];
+        int  age;
+        NSLog(@"Please enter your name: ");
+        gets(name);
+        NSLog(@"In what city do you leave?");
+        gets(city);
+        NSLog(@"Please enter how old are you: ");
+        scanf("%d", &age);
+        
+        NSLog(@"Your name is %s.", name);
+        NSLog(@"You are %d.", age);
+        NSLog(@"You live in %s.", city);
     }
     return 0;
 }

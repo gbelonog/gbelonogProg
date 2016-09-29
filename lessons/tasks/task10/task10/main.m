2b7136972facd29826803a4b2c5d4fe77dacb51f
galina <gbelonog@gmail.com>
2016-09-29 17:56:59 +0300

    tasks

---
+++ lessons/tasks/task10/task10/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  task10
+//
+//  Created by Galina on 29.09.16.
+//  Copyright (c) 2016 GalyaHome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

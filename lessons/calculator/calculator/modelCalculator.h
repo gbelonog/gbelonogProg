a5cd114f1262d54d874e5fb14e2260c0ccd5fc35
galina <gbelonog@gmail.com>
2017-01-27 11:12:07 +0200

    lessons

---
+++ lessons/calculator/calculator/modelCalculator.h
@@ -0,0 +1,13 @@
+//
+//  modelCalculator.h
+//  calculator
+//
+//  Created by Galina on 08.11.16.
+//  Copyright © 2016 GalyaHome. All rights reserved.
+//
+
+#ifndef modelCalculator_h
+#define modelCalculator_h
+
+
+#endif /* modelCalculator_h */
---
+++ lessons/calculator/calculator/modelCalculator.m
@@ -0,0 +1,9 @@
+//
+//  modelCalculator.m
+//  calculator
+//
+//  Created by Galina on 04.11.16.
+//  Copyright © 2016 GalyaHome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
